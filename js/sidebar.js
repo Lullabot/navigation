8b5de1e25f01727d97f93f9d6976cf1db6a1fab6
HARUMI JANG <harumi.jang@acquia.com>
2023-07-12 17:22:02 +0200

    Css and template changes and attach library to hook

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,20 +1,21 @@
 const sidebar = document.getElementById("navigation-sidebar");
 const sidebar_control = document.getElementById("sidebar-control");
 const dropdowns = document.getElementsByClassName('navigation-handle');
-
 // expand/collapse the sidebar
-if (sidebar_control) {
+if(sidebar_control) {
   sidebar_control.addEventListener("click", () => {
     if (sidebar.classList.contains("is-active")) {
       sidebar.classList.remove("is-active");
       sidebar_control.setAttribute("aria-label", "Expand sidebar");
       sidebar_control.setAttribute("aria-expanded", "false");
       // close all open menu items
-      var elems = document.querySelectorAll(".menu-item.menu-item--expanded");
+      var elems = document.querySelectorAll(
+        ".menu-item.menu-item--expanded");
       [].forEach.call(elems, function(el) {
-          el.classList.remove("menu-item--expanded");
-          el.querySelector('.navigation-handle[aria-expanded=true]').setAttribute['aria-expanded', 'false'];
-          el.querySelector('.action').textContent = 'Extend';
+        el.classList.remove("menu-item--expanded");
+        el.querySelector(
+          '.navigation-handle[aria-expanded=true]').setAttribute['aria-expanded', 'false'];
+        el.querySelector('.action').textContent = 'Extend';
       });
     } else {
       sidebar.classList.add("is-active");
@@ -26,7 +27,7 @@ if (sidebar_control) {
 }
 
 // expand/collapse sidebar menu dropdowns
-if (dropdowns) {
+if(dropdowns) {
   for (let i = 0; i < dropdowns.length; i++) {
     dropdowns[i].addEventListener("click", (e) => {
       const parent_list_item = e.target.parentElement.parentElement;
